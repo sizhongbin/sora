cdfe239cf8cbd8c313b53c596dd7e7f23b4fdc2e
sizhongbin <sizhongbin@hotmail.com>
2024-03-06 12:03:12 +0800

    update

--- story/js/UI.js
+++ story/js/UI.js
@@ -46,19 +46,15 @@ setup.test = function () {
         console.log(param);
         if (param.length == 2 && param[0] == 'code') {
             console.log(param[1]);
-            fetch('https://github.com/login/oauth/access_token', {
+            fetch('https://production.gitoaths.sizhongbin-b31.workers.dev/?https://github.com/login/oauth/access_token', {
                 method: 'POST',
-                body: data,
-                headers: {
-                    'client_id': 'Iv1.5a17afa660d27877',
-                    'client_secret': '567747df974b8dbeb8fbcbed80c4458513bed8de',
-                    'code': param[2],
-                    // 'Access-Control-Allow-Origin': '*',
-                    // 'Access-Control-Allow-Headers': 'Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-GitHub-OTP, X-Requested-With',
-                    // 'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE',
-                    // 'Access-Control-Expose-Headers': 'ETag, Link, X-GitHub-OTP, x-ratelimit-limit, x-ratelimit-remaining, x-ratelimit-reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes, X-Poll-Interval',
-                    // 'Access-Control-Max-Age': '86400'
-                }
+                body: {
+                    client_id: 'Iv1.5a17afa660d27877',
+                    client_secret: '567747df974b8dbeb8fbcbed80c4458513bed8de',
+                    code: param[2]
+                },
+                headers: { 'Accept': 'application/json' },
+                mode: 'cors'
             })
                 .then(function (response) {
                     if (response.ok) {
