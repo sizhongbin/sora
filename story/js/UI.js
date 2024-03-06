8c189ab0284c7c86184f88bea4cc1cebdcce8442
sizhongbin <sizhongbin@hotmail.com>
2024-03-06 17:26:32 +0800

    update

--- story/js/UI.js
+++ story/js/UI.js
@@ -33,42 +33,41 @@ setup.setMenu = function (id = 0, scene = 'test') {
 /*
  * 测试专用按钮函数
  */
-setup.test = function () {
-    let param = $(location).attr('href').split('?')[1];
-    if (!param) {
+setup.test = async function () {
+    let urlParam = $(location).attr('href').split('?')[1];
+    if (!urlParam) {
         let url = 'https://github.com/login/oauth/authorize?client_id=Iv1.5a17afa660d27877';
         if ($(location).attr('href') == 'http://127.0.0.1:1234/') url += '&redirect_uri=http://127.0.0.1:1234/';
         $(location).prop('href', url);
     }
     else {
-        console.log(param);
-        param = param.split('=');
-        console.log(param);
-        if (param.length == 2 && param[0] == 'code') {
-            console.log(param[1]);
-            fetch('https://production.gitoaths.sizhongbin-b31.workers.dev/?https://github.com/login/oauth/access_token', {
+        console.log(urlParam);
+        urlParam = urlParam.split('=');
+        console.log(urlParam);
+        if (urlParam.length == 2 && urlParam[0] == 'code') {
+            console.log(urlParam[1]);
+            const queryParam = new URLSearchParams();
+            queryParam.append('client_id', 'Iv1.5a17afa660d27877');
+            queryParam.append('client_secret', '567747df974b8dbeb8fbcbed80c4458513bed8de');
+            queryParam.append('code', urlParam[1]);
+            let postTime = new Date().getTime();
+            console.log(`postTime: ${postTime}`);
+            const response = await fetch('https://production.gitoaths.sizhongbin-b31.workers.dev/corsproxy/?apiurl=https://github.com/login/oauth/access_token', {
                 method: 'POST',
-                body: {
-                    client_id: 'Iv1.5a17afa660d27877',
-                    client_secret: '567747df974b8dbeb8fbcbed80c4458513bed8de',
-                    code: param[2]
-                },
-                headers: { 'Accept': 'application/json' },
-                mode: 'cors'
-            })
-                .then(function (response) {
-                    if (response.ok) {
-                        return response.json();
-                    } else {
-                        throw new Error('请求失败');
-                    }
-                })
-                .then(function (data) {
-                    // 处理响应结果
-                })
-                .catch(function (error) {
-                    // 处理请求失败情况
-                });
+                body: queryParam,
+                headers: { 'Accept': 'application/json' }
+            });
+            if (response.ok) {
+                const responseObj = await response.json();
+                console.log(`access_token: ${responseObj.access_token}`);
+                console.log(`expires_in: ${responseObj.expires_in}`);
+                console.log(`expires_time: ${postTime + responseObj.expires_in * 1000}`);
+                console.log(`refresh_token: ${responseObj.refresh_token}`);
+                console.log(`refresh_token_expires_in: ${responseObj.refresh_token_expires_in}`);
+                console.log(`refresh_token_expires_time: ${postTime + responseObj.refresh_token_expires_in * 1000}`);
+            } else {
+                throw new Error('请求失败');
+            }
         }
         else console.log('error');
     }
