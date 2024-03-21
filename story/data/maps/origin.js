3214dfd568b13010c41da0a6f03fa64630fa78b1
sizhongbin <sizhongbin@hotmail.com>
2024-03-21 22:34:43 +0800

    update

--- story/data/maps/origin.js
+++ story/data/maps/origin.js
@@ -1,4 +1,17 @@
-/* 地图名 */
 if (!setup.map) setup.map = new Object();
 setup.map.origin = new Object();
+
+/* 地图名 */
 setup.map.origin.name = "？？？";
+
+/* 地图类型 */
+setup.map.origin.type = "field";
+
+/* 地图开关 */
+setup.map.origin.toggle1 = false;
+
+/* 地图状态描述 */
+setup.map.origin.status = new Array();
+setup.map.origin.status.push("眼前是一片阳光下的原野。");
+setup.map.origin.status.push("地上长满了草，嫩嫩的、绿绿的，看不到边。");
+setup.map.origin.status.push("你不知道这里是哪里，但显然不是你的房间。");
