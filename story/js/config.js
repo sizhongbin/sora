b49d8f665e72ed4e1fc47b8a085238ad9d46c429
sizhongbin <sizhongbin@hotmail.com>
2024-05-13 17:45:03 +0800

    update

--- story/js/config.js
+++ story/js/config.js
@@ -1,8 +1,11 @@
-Config.saves.autosave = true; // 自动存档
-Config.saves.autoload = true; // 自动读档
+// Config.saves.autosave = true; // 自动存档
+// Config.saves.autoload = true; // 自动读档
+Config.saves.autosave = false; // 禁止自动存档
+Config.saves.slots = 3; // 最大存档数3
 Config.history.maxStates = 1; // 可回溯历史数
-Config.history.controls = false; // 不允许UI操作
-Config.passages.nobr = true;
+Config.history.controls = false; // 不允许历史操作
+Config.ui.stowBarInitially = true; // 默认收起侧边栏
+Config.passages.nobr = true; // 禁止自动换行
 Config.saves.isAllowed = function () {
   if (tags().includes('nosave')) {
     console.log('nosave');
@@ -13,14 +16,14 @@ Config.saves.isAllowed = function () {
 $(document).on(':passagedisplay', () => {
   State.expired.splice(0, Math.max(State.expired.length - 100, 0));
 }); // 限制过期历史数
-$(document).one(':storyready', function (ev) {
-  $('html').attr('data-bs-theme', 'dark');
-  $('html').addClass('mx-auto');
-  if (Save.autosave.has()) {
-    console.debug('> Force loading');
-    Save.autosave.load(); // 刷新浏览器强制读档
-    if (!tags().includes('cut')) {
-      Engine.play(setup.map[State.getVar('$gameMapKey')].id);
-    }
-  }
-});
+ $(document).one(':storyready', function (ev) {
+   $('html').attr('data-bs-theme', 'dark');
+//   $('html').addClass('mx-auto');
+//   if (Save.autosave.has()) {
+//     console.debug('> Force loading');
+//     Save.autosave.load(); // 刷新浏览器强制读档
+//     if (!tags().includes('cut')) {
+//       Engine.play(setup.map[State.getVar('$gameMapKey')].id);
+//     }
+//   }
+ });
