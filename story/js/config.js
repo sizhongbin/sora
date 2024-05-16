9e6ab39538b0399fb7902ada70160acbf1058a37
sizhongbin <sizhongbin@hotmail.com>
2024-05-16 13:24:12 +0800

    update ver.100

--- story/js/config.js
+++ story/js/config.js
@@ -8,22 +8,26 @@ Config.ui.stowBarInitially = true; // 默认收起侧边栏
 Config.passages.nobr = true; // 禁止自动换行
 Config.saves.isAllowed = function () {
   if (tags().includes('nosave')) {
-    console.log('nosave');
+    console.debug('nosave');
     return false;
   }
   return true;
 }; // 带nosave标签的passage不自动保存
 $(document).on(':passagedisplay', () => {
   State.expired.splice(0, Math.max(State.expired.length - 100, 0));
+  if (!tags().includes('nosave') && passage() != 'Title') {
+    Save.slots.save(State.getVar('$saveSlot'), State.getVar('$pcName'));
+    console.debug('Auto save to slot ' + State.getVar('$saveSlot'));
+    console.debug(Save.slots.get(State.getVar('$saveSlot')));
+  } else console.debug('Auto save disabled');
 }); // 限制过期历史数
- $(document).one(':storyready', function (ev) {
-   $('html').attr('data-bs-theme', 'dark');
-//   $('html').addClass('mx-auto');
-//   if (Save.autosave.has()) {
-//     console.debug('> Force loading');
-//     Save.autosave.load(); // 刷新浏览器强制读档
-//     if (!tags().includes('cut')) {
-//       Engine.play(setup.map[State.getVar('$gameMapKey')].id);
-//     }
-//   }
- });
+$(document).one(':storyready', function (ev) {
+  //   $('html').addClass('mx-auto');
+  //   if (Save.autosave.has()) {
+  //     console.debug('> Force loading');
+  //     Save.autosave.load(); // 刷新浏览器强制读档
+  //     if (!tags().includes('cut')) {
+  //       Engine.play(setup.map[State.getVar('$gameMapKey')].id);
+  //     }
+  //   }
+});
