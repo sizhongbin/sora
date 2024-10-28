3f95c8439a0fbfeb024cbc23b9610fb406e213e4
sizhongbin <sizhongbin@hotmail.com>
2024-10-28 17:29:03 +0800

    Add account router in test

---
+++ server/test/middleware.js
@@ -0,0 +1,16 @@
+// Change the path before deploy to PROC
+module.exports.dbPath = './test/data/test.json';
+// module.exports.dbPath = 'PROC DB Path';
+
+// Check if body is empty
+module.exports.isBodyEmpty = (req, res, next) => {
+  console.log("body: ", req.body);
+  if (Object.keys(req.body).length === 0) res.status(400).send('Empty body');
+  else next();
+}
+
+// Get current time
+module.exports.now = (time = +new Date()) => {
+  var date = new Date(time + 8 * 3600 * 1000);
+  return date.toJSON().substring(0, 23).replace('T', ' ');
+}
